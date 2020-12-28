6f6164a77fcbd8fb5caf1265757b49a45eb16a5d
f5074 <oseongryu@gmail.com>
2020-12-28 15:53:10 +0900

    -

--- cs_WCF/Services/Common/CommonService.svc.cs
+++ cs_WCF/Services/Common/CommonService.svc.cs
@@ -5,8 +5,8 @@ using System.Collections.Generic;
 using System.Data;
 
 namespace cs_WCF.Services.Common {
-    // 참고: "리팩터링" 메뉴에서 "이름 바꾸기" 명령을 사용하여 코드, svc 및 config 파일에서 클래스 이름 "PDAService"을 변경할 수 있습니다.
-    // 참고: 이 서비스를 테스트하기 위해 WCF 테스트 클라이언트를 시작하려면 솔루션 탐색기에서 PDAService.svc나 PDAService.svc.cs를 선택하고 디버깅을 시작하십시오.
+    // 참고: "리팩터링" 메뉴에서 "이름 바꾸기" 명령을 사용하여 코드, svc 및 config 파일에서 클래스 이름 "CommonService"을 변경할 수 있습니다.
+    // 참고: 이 서비스를 테스트하기 위해 WCF 테스트 클라이언트를 시작하려면 솔루션 탐색기에서 CommonService.svc나 CommonService.svc.cs를 선택하고 디버깅을 시작하십시오.
     public class CommonService : ICommonService {
         /// <summary>
         /// SelectDeptList
